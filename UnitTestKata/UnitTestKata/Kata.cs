a6edcf3510af6c7dcc565dcc06aed82e3c95677f
Daniel Underwood <cdanielunderwood@gmail.com>
2022-07-15 17:40:45 -0500

    first pass at first two katas

---
+++ UnitTestKata/UnitTestKata/Kata.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace UnitTestsKata
+{
+    public class Kata1
+    {
+        public Kata1()
+        {
+        }
+
+        public static string FizzBuzz(int input)
+        {
+            if (IsFizz(input) && IsBuzz(input))
+                return "FizzBuzz";
+            else if (IsFizz(input))
+                return "Fizz";
+            else if (IsBuzz(input))
+                return "Buzz";
+            else
+                return input.ToString();
+        }
+
+        private static bool IsFizz(int input)
+        {
+            if ((input % 3) == 0)
+                return true;
+            else
+                return false;
+        }
+
+        private static bool IsBuzz(int input)
+        {
+            if ((input % 5) == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+
+
+    public class Kata2
+    {
+        private string delimiter;
+        private string[] operands;
+        private int sum;
+
+        public Kata2()
+        {
+            sum = 0;
+            delimiter = String.Empty;
+            operands = new string[] { "1"};
+        }
+
+        public int Add(string input)
+        {
+
+
+            if (!input.Equals(String.Empty))
+            {
+
+                delimiter = parseDelimiter(input);
+                operands = parseOperands(input);
+
+                foreach (string op in operands)
+                {
+
+                    sum += System.Int32.Parse(op);
+                }
+
+            }
+
+            return sum;
+        }
+
+        private string[] parseOperands(string input)
+        {
+            return input.Substring(input.IndexOf('\n') + 1).Split(delimiter);
+        }
+
+
+        private bool validFormat(string input)
+        {
+            bool isValid = false;
+
+            if (input.StartsWith("//"))
+            {
+                if (input.Contains('\n'))
+                {
+                    int firstNewline = input.IndexOf('\n');
+                    if (firstNewline > 2)
+                    {
+                        if (input.Length > firstNewline)
+                        {
+                            int lastNewLine = input.LastIndexOf('\n');
+                            if (firstNewline == lastNewLine)
+                            {
+                                isValid = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            return isValid;
+        }
+
+        public string parseDelimiter(string input)
+        {
+            if (validFormat(input))
+            {
+                return input.Substring(2, input.IndexOf('\n') - 2);
+            }
+            else
+            {
+                throw new System.ArgumentException(string.Format("Invalid format. Valid format is //[delimeter]\\n[numbers]; actual value {0}", input), "input");
+            }
+        }
+    }
+}
---
+++ UnitTestKata/UnitTestKataTests/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿namespace UnitTestsKata1Tests
+{
+    public class UnitTestsKata1
+    {
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(3, "Fizz")]
+        [InlineData(4, "4")]
+        [InlineData(5, "Buzz")]
+        [InlineData(6, "Fizz")]
+        [InlineData(7, "7")]
+        [InlineData(10, "Buzz")]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(17, "17")]
+        [InlineData(30, "FizzBuzz")]
+        public void FizzBuzz_TestNumber(int input, string expected)
+        {
+            Assert.Equal(expected, Kata1.FizzBuzz(input));
+        }
+    }
+
+    public class UnitTestsKata2
+    {
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("//,\n1", 1)]
+        [InlineData("//,\n1,2", 3)]
+        [InlineData("//,\n2,2", 4)]
+        [InlineData("//,\n2,2,2", 6)]
+        [InlineData("//,\n1,2,3", 6)]
+        [InlineData("//;\n1;3", 4)]
+        [InlineData("//|\n1|2|3", 6)]
+        [InlineData("//sep\n2sep5", 7)]
+        public void StringCalculator_TestAdd(string input, int expected)
+        {
+            Assert.Equal(expected, new Kata2().Add(input));
+        }
+
+
+        [Theory]
+        [InlineData("//,\n2,\n3")]
+        [InlineData("//,\nbanana")]
+        [InlineData("//,\n1,2,")]
+        [InlineData("//|\n1|2,3")]
+        //[InlineData("//,\n1,-2")]
+        public void StringCalculator_TestAdd_InvalidInput(string input)
+        {
+            Assert.ThrowsAny<Exception>(() => new Kata2().Add(input));
+        }
+    }
+
+
+}
+
